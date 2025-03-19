35436d619eddfacb7a7ae6b28fa9f7eb939c9331
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-03-19 13:19:01 -0500

    feat: Read operations

--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.deleteMany({});
+// await prisma.user.deleteMany({});
 //    const user = await prisma.user.create({
 //         data:{
 //             name: "Erika",
@@ -28,17 +28,39 @@ async function main() {
             
 //         },
 // });
-    const user = await prisma.user.createMany({
-        data: [{
-            name: "Erika",
-            email: "erika@gmail.com",
-            age: 24,
-        },{
-            name: "Cristina",
-            email: "cristina@gmail.com",
-            age: 25,
-        }]
-    });
+    // const users = await prisma.user.createMany({
+    //     data: [{
+    //         name: "Erika",
+    //         email: "erika4@gmail1.com",
+    //         age: 14,
+    //     },{
+    //         name: "Erika",
+    //         email: "erika5@gmaile.com",
+    //         age: 28,
+    //     }]
+    // });
+
+    // const user = await prisma.user.findUnique({
+    //     where:{
+    //         // email:"erika@gmail.com"
+    //         age_name:{
+    //             age: 24,
+    //             name: "Erika",
+    //         }
+    //     },
+    // }); 
+    const user = await prisma.user.findMany({
+        where: {
+            name: "Erika", 
+
+        },
+        // distinct: ['name'],
+        orderBy: {
+            age: 'desc',
+        },
+        take: 2,
+        skip: 1,
+      });
     console.log(user);
 }
 
