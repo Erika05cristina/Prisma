0c1405276a8c77f3d29ba742bbd3639f75e555fd
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-03-19 13:02:34 -0500

    feat: Create client

--- script.ts
+++ script.ts
@@ -2,9 +2,44 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    // const user = await prisma.user.create({ data: { name: "Cristina" } })
-    // console.log(user)
-    await  prisma.user.deleteMany()
+    await prisma.user.deleteMany({});
+//    const user = await prisma.user.create({
+//         data:{
+//             name: "Erika",
+//             email: "erika@gmal.com",
+//             age: 24,
+//             UserPreference: {
+//                 create: {
+//                     emailUpdates: true,
+//                  },
+//              },
+//         },
+//         // include: {
+//         //     UserPreference: true,
+//         // },
+//         select: {
+//             name: true,
+//             UserPreference: {
+//                 select: {
+//                     id: true,
+//                 },
+//             }
+
+            
+//         },
+// });
+    const user = await prisma.user.createMany({
+        data: [{
+            name: "Erika",
+            email: "erika@gmail.com",
+            age: 24,
+        },{
+            name: "Cristina",
+            email: "cristina@gmail.com",
+            age: 25,
+        }]
+    });
+    console.log(user);
 }
 
 main()
